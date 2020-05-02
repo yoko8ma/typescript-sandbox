537065d712c1a0823ee06e05b158f38ef65d7fe2
Hideyuki Yokoyama <yoko8ma@users.noreply.github.com>
2020-05-03 02:57:59 +0900

    edit

--- my-app-ts/src/index.tsx
+++ my-app-ts/src/index.tsx
@@ -1,17 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class Detail extends React.Component {
+  render() {
+    return (
+      <div >
+        <div className="classification-name">名前</div>
+        <div className="description">説明</div>
+        <div className="unit-price">0円</div>
+        <div className="num-people">
+          <select value="0">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+          <span>名</span>
+        </div>
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Summary extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="party">
+          <input type="text" className="party" value="0" />
+          <span>名様</span>
+        </div>
+        <div className="total-amount">
+          <span>合計</span>
+          <input type="text" className="total-amount" value="0" />
+          <span>円</span>
+        </div>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class AdmissionFeeCalculator extends React.Component {
+  render() {
+    return (
+      <>
+        <Detail />
+        <Summary />
+      </>
+    );
+  }
+}
+
+const App: React.FC = () => {
+  return (
+    <div className="main">
+      <AdmissionFeeCalculator />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
